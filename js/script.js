8043311826dc08ef351e0015e0a308c5faa9e7cc
opect.pol@i.ua <opect.pol@i.ua>
2023-04-26 09:25:20 +0300

    start pages

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const cashSlider = new Swiper('.cash', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.cash-next',
+    prevEl: '.cash-prev',
+  },
+})
+const giftSlider = new Swiper('.giftcard', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.giftcard-next',
+    prevEl: '.giftcard-prev',
+  },
+})
+const skinsSlider = new Swiper('.skins', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.skins-next',
+    prevEl: '.skins-prev',
+  },
+})
